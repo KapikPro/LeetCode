add6ac858b5096ea825951e5bdfe518f75c28638
Yaroslav Senchin <yrik200315@gmail.com>
2024-01-27 19:39:58 +0300

    26

---
+++ Tasks/26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int size = nums.Length;
+        int max = nums[0];
+        int pointer = -1;
+        int k = 1;
+        for (int i = 1; i < size; i++)
+        {
+            if (nums[i] == max)
+            {
+                nums[i] = 0;
+                if (pointer == -1)
+                    pointer = i;
+            }
+            else
+              if (nums[i] > max)
+            {
+                k++;
+                max = nums[i];
+                if (pointer != -1)
+                {
+                    nums[pointer] = max;
+                    pointer++;
+                    nums[i] = 0;
+                }
+            }
+        }
+        return k;
+    }
+}
