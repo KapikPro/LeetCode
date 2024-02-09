28ab54f9e8c7a15ac52126b52e7cb2f8378d331b
Yaroslav Senchin <yrik200315@gmail.com>
2024-02-09 21:00:32 +0300

    1

---
+++ Tasks/121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int min = 0, razn = 0, n_razn = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < prices[min])
+                min = i;
+            n_razn = prices[i] - prices[min];
+            if (razn < n_razn)
+                razn = n_razn;
+        }
+        return razn;
+    }
+}
