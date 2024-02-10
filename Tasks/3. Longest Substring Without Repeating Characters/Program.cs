9c9ee33552ebd75e1b89330a65d68b3e72d35729
Yaroslav Senchin <yrik200315@gmail.com>
2024-02-10 20:09:56 +0300

    1

---
+++ Tasks/3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+        static int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (list.Contains(s[i]))
+                {
+                    if (list.Count > length)
+                        length = list.Count;
+                    list.RemoveRange(0, list.IndexOf(s[i])+1);
+                    list.Add(s[i]);
+                }
+                else
+                    list.Add(s[i]);
+            }
+            if (list.Count > length)
+                length = list.Count;
+            return length;
+        }
+    }
+}
