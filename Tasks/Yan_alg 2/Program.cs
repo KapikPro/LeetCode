cbf88c004111ab66bbaa11921c2a9ed2881be5df
Yaroslav Senchin <yrik200315@gmail.com>
2024-03-10 01:34:56 +0300

    solved task Yan_alg 3

--- Tasks/Yan_alg 2/Program.cs
+++ Tasks/Yan_alg 2/Program.cs
@@ -8,20 +8,43 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int sum = 0,a;
-        int count=Convert.ToInt32(Console.ReadLine());
-        for(int i=0; i<count; i++)
+        int k;
+        var line = Console.ReadLine();
+        var arr = line.Split(':');
+        int a = int.Parse(arr[0]);
+        int b = int.Parse(arr[1]);
+
+        line = Console.ReadLine();
+        arr = line.Split(':');
+        int c = int.Parse(arr[0]);
+        int d = int.Parse(arr[1]);
+
+        int sw = Convert.ToInt32(Console.ReadLine());
+
+        if (sw == 1)
         {
-            a= Convert.ToInt32(Console.ReadLine());
-            sum += a / 4;
-            a = a % 4;
-            if (a > 1 && a <= 3)
-                sum += 2;
-            else
-                if(a==1)
-                sum++;
+            k = a;
+            a = c;
+            c = k;
+
+            k = b;
+            b = d;
+            d = k;
         }
-        Console.WriteLine(sum);
+
+        if (a + c > b + d || ((a > d) && (a + c == b + d) && (a + c != 0)))
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            if (a>d && sw == 1 && a+b+c+d!=0)
+                Console.WriteLine((b + d) - (a + c));
+            else
+            {
+              Console.WriteLine((b + d) - (a + c) + 1);
+            }
+        }
     }
 }
 
