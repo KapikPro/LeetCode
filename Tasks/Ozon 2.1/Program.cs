c8c3eb6456ecc9750853403bd4c094709c1d89cd
Yaroslav Senchin <yrik200315@gmail.com>
2025-01-18 22:30:59 +0300

    Ozon 2.1

--- Tasks/Ozon 2.1/Program.cs
+++ Tasks/Ozon 2.1/Program.cs
@@ -1,6 +1,40 @@
-namespace DefaultNamespace;
+using var input = new StreamReader(Console.OpenStandardInput());
+using var output = new StreamWriter(Console.OpenStandardOutput());
 
-public class Program
+int n = int.Parse(input.ReadLine());
+for (int i = 0; i < n; i++)
 {
-    
+    output.WriteLine(Solution());
+}
+
+string Solution()
+{
+    string x = input.ReadLine();
+    if (x.Length == 1)
+        return "0";
+
+    List<int> nums = x.Select(c => int.Parse(c.ToString())).ToList();
+    x = "";
+    bool flag = false;
+
+    for (int i = 1; i < nums.Count; i++)
+    {
+        if (nums[i - 1] < nums[i] && flag == false)
+        {
+            x += "";
+            flag = true;
+        }
+        else
+            x += nums[i - 1].ToString();
+
+        if (i == nums.Count - 1)
+        {
+            if (flag == false)
+                x += "";
+            else
+                x += nums[i].ToString();
+        }
+    }
+
+    return x;
 }
