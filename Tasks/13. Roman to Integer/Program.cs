19e8e7946429ea5738b6f295780debc383cf3639
Yaroslav Senchin <93538385+KapikPro@users.noreply.github.com>
2025-12-18 02:34:02 +0300

    13. Roman to Integer

---
+++ Tasks/13. Roman to Integer/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Цель:");
+        string s = Console.ReadLine();
+        int solution = Solution.RomanToInt(s);
+        Console.WriteLine(solution);
+    }
+
+    public class Solution
+    {
+        public static int RomanToInt(string s)
+        {
+            int result = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 != s.Length)
+                {
+                    if (map[s[i]] < map[s[i + 1]])
+                        result -= map[s[i]];
+                    else
+                        result += map[s[i]];
+                }
+                else
+                {
+                    result += map[s[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
